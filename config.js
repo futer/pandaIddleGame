3c4848f7abed08a7fafb6f09d79f671406bbd783
Tomasz Jeznach <tomasz.jeznach@contractors.roche.com>
2019-10-31 13:11:34 +0100

    init

---
+++ config.js
@@ -0,0 +1,14 @@
+export const iddlespeed = 1000;
+
+export let gameTimeData = {
+    hour: 1,
+    day: 1,
+    month: 1,
+    year: 1
+};
+
+export const userData = {
+    cash: 100,
+    hourlyIncome: 1,
+    inventory: {}
+}
