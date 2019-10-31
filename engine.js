27fe8e3f42e476b14fc0da9194f8c7d46bf135df
Tomasz Jeznach <beater0sao@hotmail.com>
2019-10-31 15:01:23 +0100

    --- Auto-Git Commit ---

--- engine.js
+++ engine.js
@@ -1,15 +1,13 @@
 import { DrawImages } from './utils/drawImages.js';
+import { DrawRectangle } from './utils/drawRectangle.js';
 
 export class GameEngine {
     constructor(ctx, width, height) {
         this.ctx = ctx;
         this.width = width;
         this.height = height;
-        this.ctx.save();
-        this.ctx.fillStyle = 'black';
-        this.ctx.fillRect(0, 0, this.width, this.height);
-        this.ctx.restore();
+        new DrawRectangle(this.ctx, 0, 0, this.width, this.height, 'black', 1, true);
 
-        this.drawImage = new DrawImages(this.ctx, '', 123, 123, 1);
+        new DrawImages(this.ctx, '', 123, 123, 1);
     }
 }
