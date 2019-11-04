abbf1cdf705ccbab4af1feebb301973d2ba89f11
Tomasz Jeznach <beater0sao@hotmail.com>
2019-11-04 12:41:49 +0100

    --- Auto-Git Commit ---

--- utils/drawImages.js
+++ utils/drawImages.js
@@ -1,6 +1,6 @@
 import { gameImages } from './loadImages.js'
 
 export function drawImage(ctx, image_name, x, y, width, height) {
-    ctx.drawImage(gameImages[image_name], x, y, width, height);
+    ctx.drawImage(gameImages[image_name], x, y);
     console.log(ctx);
 }
