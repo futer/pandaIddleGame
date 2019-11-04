f822fdf54efdef5d72f26e595c94d3b7064806b8
Tomasz Jeznach <beater0sao@hotmail.com>
2019-11-04 11:41:17 +0100

    --- Auto-Git Commit ---

---
+++ utils/images_list.js
@@ -0,0 +1,17 @@
+const assets_url = './assets/images/';
+
+export const enemy_images = {
+    '': '',
+};
+
+export const texture_images = {
+    '': '',
+};
+
+export const achivment_images = {
+    '': '',
+};
+
+export const background_images = {
+    'game_background': assets_url + 'main_background.png',
+};
