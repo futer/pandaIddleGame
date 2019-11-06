84a33f07c0f608de85235e3a059d3c28ba7274a5
Tomasz Jeznach <beater0sao@hotmail.com>
2019-11-06 10:09:34 +0100

    --- Auto-Git Commit ---

--- engine.js
+++ engine.js
@@ -7,12 +7,11 @@ export class GameEngine {
         this.ctx = ctx;
         this.width = width;
         this.height = height;
-        
+        console.log(this.width, this.height);;
         
         drawImage(this.ctx, 'game_background', 0, 0, 480, 700, null);
         drawImage(this.ctx, 'enemy_island', 0, 0, 100, 100), null;
-        this.ctx.scale(0.5, 0.3);
-        new DrawMonster(this.ctx, 'red_monster', 150, 150, 990, 681, 0.5, {});
+        new DrawMonster(this.ctx, 'red_monster', (this.width  * 0.4 + this.width) / 2 - 150 , (this.height  * 0.3 + this.height) - 150, 990, 681, 0.3, {});
 
         // new DrawImages(this.ctx, '', 123, 123, 1);
     }
--- utils/drawImages.js
+++ utils/drawImages.js
@@ -4,10 +4,11 @@ export function drawImage(ctx, image_name, x, y, width, height, scale) {
     if (gameImages[image_name]['isLoaded']) {
         ctx.save();
 
-        if (scale !== null) {
+        if (scale !== null && scale !== undefined) {
+            console.log(scale);
             ctx.scale(scale, scale);
         }
-        
+
         ctx.drawImage(gameImages[image_name].image, x, y, width, height);
         ctx.restore();
     };
