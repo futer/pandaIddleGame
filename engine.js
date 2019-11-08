757c8446d37fb1564412803f3bef2875f7f86d17
Tomasz Jeznach <beater0sao@hotmail.com>
2019-11-08 10:47:01 +0100

    --- Auto-Git Commit ---

--- engine.js
+++ engine.js
@@ -1,8 +1,8 @@
 import { drawImage } from './utils/drawImages.js';
 import { enemy_list } from './utils/enemyList.js';
 import { DrawMonster } from './utils/drawMonster.js';
-import { DrawRectangle } from './utils/drawRectangle.js';
 import { DrawText } from './utils/drawText.js';
+import { actionManagement } from './utils/actionManagement.js';
 
 export class GameEngine {
     constructor(ctx, canvas, width, height) {
@@ -10,20 +10,25 @@ export class GameEngine {
         this.canvas = canvas;
         this.width = width;
         this.height = height;
-        
+        this.interval = 1000/60
+
         drawImage(this.ctx, 'game_background', 0, 0, 480, 700, null);
 
         this.canvas.addEventListener('click', (event) => {
-            if (event.layerX >= 75 && event.layerX <= 340 && event.layerY >= 180 && event.layerY <= 530) {
-                console.log('attacked');
-            }
+            actionManagement(event, 'attack_monster');
         });
 
+        setInterval(() => {
 
-        enemy_list.forEach(element => {
-            new DrawMonster(this.ctx, 'red_monster', (this.width / 2) - (element.width / 2) , (this.height / 2) - (element.height / 2), element.width, element.height, null, {element});
-            new DrawText(this.ctx, (this.width / 2), (this.height /1.3)).drawText(`${element.losthp}/${element.hp}`, 'black', 'Arial', 40, false);
-        });
+            
+            enemy_list.forEach(element => {
+                new DrawMonster(this.ctx, 'red_monster', (this.width / 2) - (element.width / 2) , (this.height / 2) - (element.height / 2), element.width, element.height, null, {element});
+                new DrawText(this.ctx, (this.width / 2), (this.height /1.3)).drawText(`${element.losthp}/${element.hp}`, 'black', 'Arial', 40, false);
+            });
+        }, this.interval);
+
+
+        
 
         
 
--- utils/actionManagement.js
+++ utils/actionManagement.js
@@ -1,5 +1,13 @@
-export class ActionManagement {
-    constructor() {
-        
+export function actionManagement(event, type) {
+
+    switch (true) {
+        case type === 'attack_monster':
+            if (event.layerX >= 75 && event.layerX <= 340 && event.layerY >= 180 && event.layerY <= 530) {
+                console.log('attacked');
+            };
+            break;
+
+        default:
+            break;
     }
 };
