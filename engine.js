276505b106857ec4ea3d463f7d1b77291483bd7d
Tomasz Jeznach <beater0sao@hotmail.com>
2019-11-26 11:42:25 +0100

    --- Auto-Git Commit ---

--- engine.js
+++ engine.js
@@ -149,6 +149,7 @@ export class GameEngine {
             ...playerData,
             attack: addedAttack,
         }));
+        playerOptions.attack = addedAttack;
         this.getGamaData();
     }
 
