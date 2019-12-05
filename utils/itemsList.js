37c3be996ed534d40b9f7eac32d87d14c25edab6
Tomasz Jeznach <beater0sao@hotmail.com>
2019-12-05 09:18:52 +0100

    --- Auto-Git Commit ---

---
+++ utils/itemsList.js
@@ -0,0 +1,12 @@
+export const itemsList = [
+    {
+        name: 'wooden_sword',
+        damage: 10,
+        costs: 100
+    },
+    {
+        name: 'copper_sword',
+        damage: 25,
+        costs: 250,
+    }
+];
