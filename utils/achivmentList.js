996634c0045239271376236031bffcf003ec3764
Tomasz Jeznach <beater0sao@hotmail.com>
2019-12-06 15:13:03 +0100

    --- Auto-Git Commit ---

--- utils/achivmentList.js
+++ utils/achivmentList.js
@@ -1,1 +1,58 @@
-export const Achievement
+export const AchievementList = [
+    {
+        name: 'GoldColectionLevel1',
+        prize: 1000,
+        value: 2000,
+        status: false,
+        isHidden: true,
+    },
+    {
+        name: 'GoldColectionLevel2',
+        prize: 5000,
+        value: 10000,
+        status: false,
+        isHidden: true,
+    },
+    {
+        name: 'GoldSpentLevel1',
+        prize: null,
+        value: 1000,
+        status: false,
+        isHidden: false,
+    },
+    {
+        name: 'GoldSpentLevel2',
+        prize: null,
+        value: 5000,
+        status: false,
+        isHidden: false,
+    },
+    {
+        name: 'PressKeyNoErrorLevel1',
+        prize: 1000,
+        value: 15,
+        status: false,
+        isHidden: false,
+    },
+    {
+        name: 'PressKeyNoErrorLevel1',
+        prize: 5000,
+        value: 50,
+        status: false,
+        isHidden: false,
+    },
+    {
+        name: 'DamageStatusLevel1',
+        prize: 100,
+        value: 1000,
+        status: false,
+        isHidden: false,
+    },
+    {
+        name: 'DamageStatusLevel2',
+        prize: 1000,
+        value: 10000,
+        status: false,
+        isHidden: false,
+    }
+];
