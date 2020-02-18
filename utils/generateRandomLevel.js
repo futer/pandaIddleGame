58ebc640cf30539dadc49a30a4532c6ada178365
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-18 11:53:38 +0100

    --- Auto-Git Commit ---

--- utils/generateRandomLevel.js
+++ utils/generateRandomLevel.js
@@ -4,10 +4,16 @@ import { playerOptions } from './playerOptions.js';
 
 export const platform_number = Math.floor(Math.random() * isle_list.length) + 1;
 export const monsterID = Math.floor(Math.random() * enemy_list.length);
-console.log(monsterID);
 export const monsterObject = enemy_list[monsterID];
 export const monsterName = monsterObject.monster_name;
 
+export function generateRandomMonster() {
+    let monsterID = Math.floor(Math.random() * enemy_list.length);
+    let monsterObject = enemy_list[monsterID];
+    function genereateMonsterName() {
+        return monsterObject.monster_name;
+    }
+}
 
 export function generateRandomLevel() {
     const increaseHP = 10
@@ -19,11 +25,11 @@ export function generateRandomLevel() {
 
     const newLevel = {
 
-        losthp: !isMonsterBoss ? _hp + ((currentLevel + 1 ) * increaseHP) : _hp + ((currentLevel + 1) * (increaseHP * 4)),
-        hp: !isMonsterBoss ? _hp + ((currentLevel + 1 )* increaseHP) :  _hp + ((currentLevel + 1) * (increaseHP * 4)),
+        losthp: !isMonsterBoss ? _hp + ((currentLevel + 1) * increaseHP) : _hp + ((currentLevel + 1) * (increaseHP * 4)),
+        hp: !isMonsterBoss ? _hp + ((currentLevel + 1) * increaseHP) : _hp + ((currentLevel + 1) * (increaseHP * 4)),
 
         min_gold: isMonsterBoss ? currentLevel * 2 : currentLevel,
-        max_gold:  isMonsterBoss ? ((currentLevel * 2) + 10) : (currentLevel + 3),
+        max_gold: isMonsterBoss ? ((currentLevel * 2) + 10) : (currentLevel + 3),
 
         width: 300,
         height: 206.33,
