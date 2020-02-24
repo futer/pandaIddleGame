0e78f9b614650ce3ac9dc7bedafc4ec48b4fd9fd
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-24 14:08:05 +0100

    --- Auto-Git Commit ---

--- utils/loginPage.js
+++ utils/loginPage.js
@@ -9,41 +9,49 @@ export function createLoginPage() {
 
 
         firebase.auth()
-                .signInWithEmailAndPassword(login, password)
-                .then((event) => {
-                    alert(event)
-                }).catch((error) => {
+            .signInWithEmailAndPassword(login, password)
+            .then((event) => {
+                isLogged = true;
+                hideForms();
+            }).catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                alert(errorCode + ": " + errorMessage);
+            });
+    });
+
+    const registerForm = document.getElementById('registerForm');
+    registerForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+
+        let login = evt.target[0].value;
+        let password = evt.target[1].value
+        let repassword = evt.target[2].value
+
+        if (password === repassword) {
+            firebase.auth()
+                .createUserWithEmailAndPassword(login, password)
+                .then(function (user) {
+                    if (user !== null || user !== undefined) {
+                        alert(`Create user : ${user.user.email}`);
+                        isLogged = true;
+                        hideForms();
+                    }
+                })
+                .catch(function (error) {
                     var errorCode = error.code;
                     var errorMessage = error.message;
-                    alert(errorCode + ": " + errorMessage);
+
+                    window.alert("There went something wrong : " + errorCode + ')' + errorMessage);
                 });
+        } else {
+            alert('passwords are not the same');
+        }
     })
+}
 
-    // document.getElementById('login').addEventListener('click', () => {
-    //     let getEmail = document.getElementById('email_login').value;
-    //     let getPassword = document.getElementById('password_login').value;
-    //     console.log(getEmail + getPassword);
-    // });
-    // button_register.addEventListener('click', (evt) => {
-    //     const email = document.getElementsByClassName('email_input')[0].value;
-    //     const password = document.getElementsByClassName('password_input')[0].value;
-
-    //     firebase.auth()
-    //         .createUserWithEmailAndPassword(email, password)
-    //         .then(function (user) {
-    //             if (user !== null || user !== undefined) {
-    //                 window.alert(`Create user : ${user.user.email}`);
-    //                 isLogged = true;
-    //             }
-    //             // var user = firebase.auth().currentUser;
-    //         })
-    //         .catch(function (error) {
-    //             var errorCode = error.code;
-    //             var errorMessage = error.message;
-
-    //             window.alert("There went something wrong : " + errorMessage);
-    //         });
-    // });
-
-
+function hideForms() {
+    const forms = document.getElementsByClassName('container')[0];
+    forms.style.display = 'none';
+    document.body.classList.remove('auth_background');
 }
