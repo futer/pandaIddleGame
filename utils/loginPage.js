defa006ecd315e1b1b4916543fdebbcd5117fe4c
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-24 10:46:20 +0100

    --- Auto-Git Commit ---

--- utils/loginPage.js
+++ utils/loginPage.js
@@ -15,12 +15,23 @@ export function createLoginPage() {
     button_register.addEventListener('click', (evt) => {
         const email = document.getElementsByClassName('email_input')[0].value;
         const password = document.getElementsByClassName('password_input')[0].value;
-        console.log(email, password);
-    })
-    
+
+        firebase.auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(function (user) {
+                alert(user);
+                // var user = firebase.auth().currentUser;
+            })
+            .catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                window.alert("There went something wrong : " + errorMessage);
+            });
+    });
+
     body.appendChild(email_input);
     body.appendChild(password_input);
     body.appendChild(button_register);
 
 }
-
