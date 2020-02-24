583c9c541c4267a74408cf303238c35b3effbf1d
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-24 11:56:57 +0100

    --- Auto-Git Commit ---

--- utils/loginPage.js
+++ utils/loginPage.js
@@ -4,18 +4,6 @@ const body = document.body;
 export let isLogged = false;
 
 export function createLoginPage() {
-
-    const email_input = document.createElement('input');
-    email_input.className = 'email_input';
-
-    const password_input = document.createElement('input');
-    password_input.className = 'password_input';
-    password_input.type = 'password';
-
-    const button_register = document.createElement('button');
-    button_register.innerText = 'register'
-    button_register.className = 'register_button';
-
     button_register.addEventListener('click', (evt) => {
         const email = document.getElementsByClassName('email_input')[0].value;
         const password = document.getElementsByClassName('password_input')[0].value;
@@ -41,13 +29,4 @@ export function createLoginPage() {
     body.appendChild(password_input);
     body.appendChild(button_register);
 
-}
-
-export function deleteRegisterElement() {
-    let elm1 = document.getElementsByClassName('email_input')[0]
-    let elm2 = document.getElementsByClassName('password_input')[0]
-    let elm3 = document.getElementsByClassName('register_button')[0]
-    elm1.parentNode.removeChild(elm1);
-    elm2.parentNode.removeChild(elm2);
-    elm3.parentNode.removeChild(elm3);
 }
