250550d9ae9581217965e44b31d5679d882c50b6
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-24 09:55:54 +0100

    --- Auto-Git Commit ---

--- utils/loginPage.js
+++ utils/loginPage.js
@@ -2,8 +2,25 @@ export function createLoginPage() {
     const body = document.body;
 
     const email_input = document.createElement('input');
-    email_input.className = 'email';
+    email_input.className = 'email_input';
+
+    const password_input = document.createElement('input');
+    password_input.className = 'password_input';
+    password_input.type = 'password';
+
+    const button_register = document.createElement('button');
+    button_register.innerText = 'register'
+    button_register.className = 'register_button';
+
+    button_register.addEventListener('click', (evt) => {
+        const email = document.getElementsByClassName('email_input')[0].value;
+        const password = document.getElementsByClassName('password_input')[0].value;
+        console.log(email, password);
+    })
     
     body.appendChild(email_input);
+    body.appendChild(password_input);
+    body.appendChild(button_register);
+
 }
 
