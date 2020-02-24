66f6b774bd9d1f698cc66cd31fede7fb669a6e61
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-24 09:35:43 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import { GameEngine } from './engine.js';
 import { LoadImages, gameImages } from './utils/loadImages.js';
 import { background_images } from './utils/images_list.js';
-
+import { firebase_config } from './config.js';
 class InitGame {
     constructor() {
+
+        firebase.initializeApp(firebase_config);
+        firebase.analytics();
+
+        firebase.auth().createUser({
+            email: 'user@example.com',
+            emailVerified: false,
+            phoneNumber: '+11234567890',
+            password: 'secretPassword',
+            displayName: 'John Doe',
+            photoURL: 'http://www.example.com/12345678/photo.png',
+            disabled: false
+        }).then(function (userRecord) {
+            console.log('Successfully created new user:', userRecord.uid);
+        }).catch(function (error) {
+            console.log('Error creating new user:', error);
+        });
+
+
+
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
 
@@ -16,7 +36,7 @@ class InitGame {
         document.body.appendChild(this.canvas);
 
         new LoadImages();
-        
+
         let checkImages = setInterval(() => {
             const keys = Object.entries(gameImages);
             this.loadedImages = [];
