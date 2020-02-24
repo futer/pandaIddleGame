46f00d09b34f97c77acd9d27095dc8d69fbbb0fc
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-24 13:07:34 +0100

    --- Auto-Git Commit ---

--- utils/loginPage.js
+++ utils/loginPage.js
@@ -1,11 +1,29 @@
 export let isLogged = false;
 
 export function createLoginPage() {
-    document.getElementById('login').addEventListener('click', () => {
-        let getEmail = document.getElementById('email_login').value;
-        let getPassword = document.getElementById('password_login').value;
-        console.log(getEmail + getPassword);
-    });
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        let login = evt.target[0].value;
+        let password = evt.target[1].value
+
+
+        firebase.auth()
+                .signInWithEmailAndPassword(login, password)
+                .then((event) => {
+                    alert(event)
+                }).catch((error) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    alert(errorCode + ": " + errorMessage);
+                });
+    })
+
+    // document.getElementById('login').addEventListener('click', () => {
+    //     let getEmail = document.getElementById('email_login').value;
+    //     let getPassword = document.getElementById('password_login').value;
+    //     console.log(getEmail + getPassword);
+    // });
     // button_register.addEventListener('click', (evt) => {
     //     const email = document.getElementsByClassName('email_input')[0].value;
     //     const password = document.getElementsByClassName('password_input')[0].value;
