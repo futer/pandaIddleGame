7453185b72257500cc7a0b8407369a491bf0de50
Tomasz Jeznach <beater0sao@hotmail.com>
2020-02-26 14:23:21 +0100

    --- Auto-Git Commit ---

--- utils/gameData.js
+++ utils/gameData.js
@@ -7,22 +7,24 @@ export function getGamaData() {
 
 export function setGameData() {
     console.log('launche');
-    if (getFromLocalStorage('player_data') === undefined || getFromLocalStorage('player_data') === null) {
-        const playerData = getFromDBPlayerData();
-        console.log(playerData);
-        if (playerData === null || playerData === undefined) {
-            console.log('2');
-            saveToLocalStorage('player_data', JSON.stringify(playerOptions));
-            savetToDB(userID, playerOptions);
-        } else {
-            console.log('3');
-            getFromDBPlayerData()
-        };
-    }
-    else {
-        console.log('4');
-        getFromDBPlayerData();
-    }
+    const test = localStorage.getItem('user');
+    console.log(test);
+    // if (getFromLocalStorage('player_data') === undefined || getFromLocalStorage('player_data') === null) {
+    //     const playerData = getFromDBPlayerData();
+    //     console.log(playerData);
+    //     // if (playerData === null || playerData === undefined) {
+    //     //     console.log('2');
+    //     //     saveToLocalStorage('player_data', JSON.stringify(playerOptions));
+    //     //     savetToDB(userID, playerOptions);
+    //     // } else {
+    //     //     console.log('3');
+    //     //     getFromDBPlayerData()
+    //     // };
+    // }
+    // else {
+    //     console.log('4');
+    //     getFromDBPlayerData();
+    // }
 }
     // if (getFromLocalStorage('player_data') === undefined || getFromLocalStorage('player_data') === null) {
     //     saveToLocalStorage('player_data', JSON.stringify(playerOptions));
