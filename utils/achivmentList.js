8a0b0a6fa2e0a2d06e5032755c1edf1b24fc5a06
Tomasz Jeznach <beater0sao@hotmail.com>
2020-03-04 10:25:03 +0100

    --- Auto-Git Commit ---

--- utils/achivmentList.js
+++ utils/achivmentList.js
@@ -1,17 +1,22 @@
 export const achievementList =  {
     1: [
         {
-            name: 'GoldColectionLevel1',
+            id: 'GoldColectionLevel1',
+            name: 'Collect 50 gold',
             prizeType: 'gold',
             prizeValue: 100,
+            
             target: 50,
+            isComplete: false,
             isHidden: false,
         },
         {
-            name: 'GoldColectionLevel2',
+            id: 'GoldColectionLevel2',
+            name: 'Collect 100 gold',
             prizeType: 'gold',
             prizeValue: 200,
             target: 100,
+            isComplete: false,
             isHidden: false,
         },
     ],
